70b6728e00811dc66e95d3744ede8587d553440c
NamMinwu <minwu06255@gmail.com>
2023-01-28 14:02:24 +0900

    custom image done

--- custom_image_dataset.py
+++ custom_image_dataset.py
@@ -7,6 +7,7 @@ import torch
 from torch.utils.data import Dataset
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
 
 training_data = datasets.FashionMNIST(
     root='data',
@@ -21,32 +22,44 @@ test_data = datasets.FashionMNIST(
     download=True,
     transform=ToTensor()
 )
-print(training_data)
 
-class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
-        self.img_dir = img_dir
-        self.transform = transform
-        self.target_transform = target_transform
+# class CustomImageDataset(Dataset):
+#     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+#         self.img_labels = pd.read_csv(annotations_file)
+#         self.img_dir = img_dir
+#         self.transform = transform
+#         self.target_transform = target_transform
 
-    def __len__(self):
-        return len(self.img_labels)
+#     def __len__(self):
+#         return len(self.img_labels)
 
-    def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])##img_path make 
-        image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]##
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        sample = {"image": image, "label": label}
-        return sample
+#     def __getitem__(self, idx):
+#         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])##img_path make 
+#         image = read_image(img_path)
+#         label = self.img_labels.iloc[idx, 1]##
+#         if self.transform:
+#             image = self.transform(image)
+#         if self.target_transform:
+#             label = self.target_transform(label)
+#         sample = {"image": image, "label": label}
+#         return image, sample
 
 
 
 ##dataloadere
 train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
 test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+print(train_dataloader)
 
+#DataLoader iterate
+train_features, train_labels = next(iter(train_dataloader))
+# print(f"Feature batch shape: {train_features.size()}")
+# print(f"Labels batch shape: {train_labels.size()}")
+img = train_features[0].squeeze()
+# label = train_labels[0]
+
+plt.imshow(img, cmap="gray")
+plt.show()
+# # print(img)
+# # print(label)
+# print(f"Label: {label}")
